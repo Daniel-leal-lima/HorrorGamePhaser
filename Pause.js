46dc0eee19543b56d0eb8251d4cd7ce06ca133a7
fkiyoshii <fkiyoshii@.github.com>
2019-11-28 19:49:48 -0300

    menus pause-game over

--- Pause.js
+++ Pause.js
@@ -2,6 +2,11 @@ class Pause extends Phaser.Scene {
     constructor() {
         super({key:'pause'});
     }
+    preload(){
+    	this.load.image('pauseimage','img/pause.png' )
+    }
+
+
     create(){
         this.keyObj = this.input.keyboard.addKey('P');
         console.log('to pausado!');
@@ -10,5 +15,15 @@ class Pause extends Phaser.Scene {
              this.scene.resume('jogo');
              this.scene.stop();
         },this);
+
+        let pauseimage = this.make.sprite({
+        x:300,
+        y:100,
+        scale:.5,
+        key:'pauseimage'
+      //  pauseimage.setAlpha(.3);
+
+        
+	});
     }
 }
